25159132b814f0016b736585f6d92610561f54dd
Tyler Guillen <tyguillen@gmail.com>
2018-04-28 18:13:11 -0700

    remove global

--- lib/flickraw.rb
+++ lib/flickraw.rb
@@ -13,10 +13,3 @@ module FlickRaw
   self.secure = true
   self.check_certificate = true
 end
-
-# Use this to access the flickr API easily. You can type directly the flickr requests as they are described on the flickr website.
-#  require 'flickraw'
-#
-#  recent_photos = flickr.photos.getRecent
-#  puts recent_photos[0].title
-def flickr; $flickraw ||= FlickRaw::Flickr.new end
